e425f57675a6cea0538ff2a25610fba34e850955
BartoszKwec <bartosz.kwec@softwarehut.com>
2025-11-07 14:50:41 +0100

    Init commit

---
+++ src/main/java/pl/kwec/mymanagerplannerservice/MymanagerPlannerServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.kwec.mymanagerplannerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymanagerPlannerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymanagerPlannerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kwec/mymanagerplannerservice/MymanagerPlannerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwec.mymanagerplannerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymanagerPlannerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
